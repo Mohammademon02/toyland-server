bdfcb9590686c67707929e4c0e749ebf1b8850de
mohammademon02 <mohammademon0207@gmail.com>
2023-05-21 12:30:19 +0600

    mongoDB env completed

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-
-//mohammademon0207
-//G7pKnRcduD3vnKOR
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -13,8 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mohammademon0207:<password>@cluster0.b1uj4ox.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b1uj4ox.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
