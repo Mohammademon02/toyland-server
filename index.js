f11d2e3c98afeac828190aff547dacab5f689e9f
mohammademon02 <mohammademon0207@gmail.com>
2023-05-26 01:59:37 +0600

    toy category section completed

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b1uj4ox.mongodb.net/?retryWrites=true&w=majority`;
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -27,12 +26,67 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+        const toysCollection = client.db('toyLand').collection('allToys');
+
+        // app.get('/allToys', async (req, res) => {
+            
+        //     const data = req.query;
+        //     const category = req.query.category;
+        //     const search = req.query.search;
+            
+        //     let query = {};
+        //     if (category) {
+        //         query = {sub_category: category}
+        //     }
+        //     if(search){
+        //         query= {name: {regex:search, options: "i"}}
+        //     }
+
+
+
+        //     const result = await toysCollection
+        //         .find(query)
+        //         .toArray();
+        //     res.send(result);
+        // })
+
+        app.get("/allToy", async (req, res) => {
+            const result = await toysCollection.estimatedDocumentCount();
+            res.send({ result });
+        });
+
+
+
+        app.get("/allToys", async (req, res) => {
+            const data = req.query;
+
+            const category = req.query.category;
+
+            let query = {};
+
+            if (category) {
+                query = { sub_category: category };
+            }
+
+            const result = await toysCollection
+                .find(query)
+                .toArray();
+            res.send(result);
+        });
+
+
+        app.post('/allToys', async (req, res) => {
+            const addToy = req.body;
+            const result = await toysCollection.insertOne(addToy);
+            res.send(result);
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
